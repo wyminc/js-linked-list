a99b8f83fea34c389545b7d6f9f34b459fc26bf9
wyminc <wyminc808@gmail.com>
2018-07-23 19:04:08 -1000

    Cleaned up commented old code and added real comments to explain while loops

--- linkedList.js
+++ linkedList.js
@@ -6,17 +6,6 @@
 function linkedListGenerator() {
   let head = null;
   let tail = null;
-  //This count will have two potential uses. One is to have an index property in my object, and another is to have an actual count that will be a measurement of "length" of the whole list
-  // let count = 0;
-
-  // class Node {
-  //   constructor(value) {
-  //     this.value = value;
-  //     this.next = null;
-  //     //Now whenever I want to extentiate the new node, it will have a property called position equal to the count. Because teh count is increasing whnever I add a new node, the position will be dynamic
-  //     this.position = count;
-  //   }
-  // }
 
   const getHead = () => {
     return head;
@@ -30,33 +19,16 @@ function linkedListGenerator() {
     const newNode = {
       value: data,
       next: null
-      // position: count
     };
 
     if (head === null) {
       head = newNode;
       tail = newNode;
-
-      //The count increment is after the creation of the node because we want our starting node to have a position of 0. That is how the test is testing it. The first node it is looking for will be with a nth value of 0 not 1.
     } else {
       tail.next = newNode;
       tail = tail.next;
     }
-    // count++;
     return tail;
-
-    // if (head !== null && tail.next === null) {
-    //   tail.next = newNode;
-    //   tail = newNode;
-    //   //The count increment is after the creation of the node because we want our starting node to have a position of 0. That is how the test is testing it. The first node it is looking for will be with a nth value of 0 not 1.
-    //   count++;
-    //   return tail;
-    // } else {
-    //   head = newNode;
-    //   tail = newNode;
-    //   count++;
-    //   return tail;
-    // }
   };
 
   const get = number => {
@@ -65,8 +37,11 @@ function linkedListGenerator() {
     let counter = 0;
 
     while (searchNode) {
+      //Simple if to look if the number(counter) that the loop is at is equal to the number that is being inputted(nubmer)
       if (counter === number) {
         return searchNode;
+
+        //This if is where the loop will continue to iteriate through each node until the counter is equal to the number
       } else if (counter !== number && searchNode.next !== null) {
         counter++;
         searchNode = searchNode.next;
@@ -74,15 +49,6 @@ function linkedListGenerator() {
         return false;
       }
     }
-    // while (searchNode) {
-    //   if (searchNode.position === number) {
-    //     return searchNode;
-    //   } else if (searchNode.position !== number && searchNode.next !== null) {
-    //     searchNode = searchNode.next;
-    //   } else {
-    //     return false;
-    //   }
-    // }
   };
 
   const remove = number => {
@@ -91,59 +57,34 @@ function linkedListGenerator() {
     let counter = 0;
 
     while (searchNode) {
-      if (counter === 0 && number === 0 && searchNode.next !== null) {
+      //This is the first condition in the ifs to check if the node that wants to be removed will be in the head position
+      //This if condition is in the beginning because we want our loop to search for the head first while the counter is at 0 before it starts to increase
+      if (counter === 0 && number === 0) {
         head = searchNode.next;
         return head;
+
+        //The loop will always want to be at the node before the actual node it wants to remove, hence the number - 1(loop is at 0 position but wants to remove 1, we need node0.next to affect 1)
+        //There is also a check to see if the node that is after the current node(the node that is trying to be removed) exists
       } else if (counter === number - 1 && searchNode.next !== null) {
-        searchNode.next = searchNode.next.next;
+        searchNode.next = searchNode.next.next; //set the searchNode.next to equal the node after it, essentially removing the original searchNode.next
+
+        //This is an inner if statement within the elseif that will check to see if the node that was just changed is the last node
         if (searchNode.next === null) {
           tail = searchNode;
         }
         return tail;
+
+        //This if is where the loop will continue to iteriate through each node until the counter is equal to the number-1.
+        //eg if the loop is trying to remove 3, it needs to equal 2 first and until that point searchNode will keep changing to searchNode.next, searchNode.next.next, searchNode.next.next.next, etc.
       } else if (counter !== number - 1 && searchNode.next !== null) {
         counter++;
         searchNode = searchNode.next;
+
+        //Else return false for the test to work
       } else {
         return false;
       }
     }
-
-    // var searchNode = head;
-    // console.log(searchNode, "Search Node");
-    // console.log(searchNode.next, "Search Node Next");
-    // console.log(searchNode.next.next, "Search Node Next Next");
-    // console.log(number, "WHAT NUMBER BRUH");
-    // while (searchNode) {
-    //   if (
-    //     searchNode.position === number - 1 &&
-    //     searchNode.next !== null &&
-    //     count - searchNode.position > 3
-    //   ) {
-    //     searchNode.next = searchNode.next.next;
-    //     count--;
-    //     for (var i = searchNode.position; i < count; i++) {
-    //       searchNode.next.position = searchNode.next.position - 1;
-    //       searchNode = searchNode.next;
-    //     }
-    //     return searchNode.next;
-    //   } else if (
-    //     searchNode.position === number - 1 &&
-    //     searchNode.next !== null
-    //   ) {
-    //     console.log("Second if when its just limited");
-    //     console.log(searchNode, "This should be Mozilla");
-    //     console.log(searchNode.next, "This should be ");
-    //     searchNode.next = searchNode.next.next;
-    //     searchNode.next.position = searchNode.next.position - 1;
-    //     count--;
-    //     return searchNode.next;
-    //   } else if (searchNode.position !== number && searchNode.next !== null) {
-    //     console.log("IM SWITCHING IT UP");
-    //     searchNode = searchNode.next;
-    //   } else {
-    //     return false;
-    //   }
-    // }
   };
 
   const insert = (data, number) => {
@@ -152,31 +93,39 @@ function linkedListGenerator() {
     let counter = 0;
 
     while (searchNode) {
-      let insertNode = {
+      //This will be variable used to create new nodes(objects) everytime the method is called.
+      const insertNode = {
         value: data,
         next: null
       };
 
-      if (counter === 0 && number === 0 && searchNode.next !== null) {
+      //This is the first condition in the ifs to check if the node that wants to be added will be in the head position
+      //This if condition is in the beginning because we want our loop to search for the head first while the counter is at 0 before it starts to increase
+      if (counter === 0 && number === 0) {
         head = insertNode;
         head.next = searchNode;
         return head;
+
+        //The loop will always want to be at the node before the actual node it wants to add, hence the number - 1(loop is at 0 position but wants to add a node before 1, we need node0.next to make it the new node1)
+        //There is also a check to see if the node that is after the current node exists
       } else if (counter === number - 1 && searchNode.next !== null) {
         savedNode = searchNode.next;
-
-        searchNode.next = null;
         searchNode.next = insertNode;
-
         insertNode.next = savedNode;
 
+        //If the node after the current node does not exist, then it will become the tail
         if (searchNode.next.next === null) {
           tail = searchNode.next;
         }
-
         return tail;
+
+        //This if is where the loop will continue to iteriate through each node until the counter is equal to the number-1.
+        //eg if the loop is trying to insert at 3, it needs to equal 2 first and until that point searchNode will keep changing to searchNode.next, searchNode.next.next, searchNode.next.next.next, etc.
       } else if (counter !== number - 1 && searchNode.next !== null) {
         counter++;
         searchNode = searchNode.next;
+
+        //Else return false for the test to work
       } else {
         return false;
       }
